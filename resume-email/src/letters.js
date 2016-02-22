b32075c19e002727b68a1eecf70a903032b9638a
Alexgalinier <contact@alexgalinier.com>
2016-02-22 20:24:05 +0100

    Set letters

--- resume-email/src/letters.js
+++ resume-email/src/letters.js
@@ -184,4 +184,39 @@ module.exports.letters = [
     "pdf_link_text": "If my resume is not available or has been blocked, you can download it at :",
     "pdf_link": "http://alexgalinier.com/static/documents/Alex_Galinier_Resume_En_Mail.pdf"
   },
+
+  {
+    "name":    "fyte-motivation",
+    "lang":    "fr",
+    "title":   "Alex Galinier | FYTE, lettre de motivation",
+    "visu":    "Visualiser cet email dans votre navigateur",
+    "age":     "28 ans",
+    "subject": "MADAME, MONSIEUR, BONJOUR,",
+    "date":    "Le 08 Avril 2015",
+    "prg_1":   "Avec 6 ans d'expériences dans le développement de produits web et mobile, la conduite de projet, et la faculté de résoudre des problèmes en temps et en heure, " +
+      "je me permets de vous proposer ma candidature au poste de Chef de projet / développeur Front-End.",
+    "prg_2":   "J'ai travaillé à la réalisation et la livraison de nombreux projets, allant du simple POC (Prouf Of Concept) à la plateforme multi-services constament connectée, ainsi je connais l'importance de la gestion du temps et d'une communication claire.<br/> En plus d'affûter mes compétences techniques, j'ai aimé intervenir à tous les niveaux de ces projets, apportant mes connaissances, mes idées et des alternatives à mes collègues ce qui a orienté mon développement professionnel. Au fil de ma carrière j'ai ainsi pu :<br/>- Améliorer la relation client et la conduite du projet en implémentant une méthode inspirée de l'Agilité<br/>- Participer à la création d'une organisation<br/>- Transformer un portail applicatif pour tendre vers les besoins des utilisateurs et des clients",
+    "prg_3":   "Je me tiens informé quotidiennement des idées, méthodologies ou nouvelles technologies afin d'élargir ou approfondir mes connaissances. Je crois en cette recherche perpétuelle de savoir, ainsi intégrer votre entreprise est un réel enjeux d'avenir pour lequel j'espère pouvoir vous apporter mes compétences actuelles et futures.<br/>Vous pouvez me joindre sur mon mobile ou par mail.",
+    "prg_4":   "J'espère pouvoir vous rencontrer pour échanger à propos de cette opportunité.",
+    "final":   "SINCÈREMENT",
+    "pdf_link_text":"Si jamais mon CV en pièce jointe n'est pas disponible ou a été bloqué, vous pouvez le télécharger sur :",
+    "pdf_link": "http://alexgalinier.com/static/documents/Alex_Galinier_Resume_Fr_Mail.pdf"
+  },
+
+  {
+    "name":    "wax-motivation",
+    "lang":    "fr",
+    "title":   "Alex Galinier | Wax, lettre de motivation",
+    "visu":    "Visualiser cet email dans votre navigateur",
+    "age":     "28 ans",
+    "subject": "MADAME BRICE, BONJOUR,",
+    "date":    "Le 28 Avril 2015",
+    "prg_1":   "Nous nous sommes déjà contacté pour le poste de développeur front-end, et à l'époque je n'avais pas vu l'offre d'architecte front-end, même si vous me l'aviez évoquée.",
+    "prg_2":   "Je suis très intéressé pour travailler dans votre entreprise et ce poste d'architecte correspond tout à fait à mes ambitions professionnelles.",
+    "prg_3":   "C'est pourquoi je me permet de postuler également à cette offre et espère pouvoir vous démontrer ma motivation et mes compétences lors d'un futur entretien.",
+    "prg_4":   "",
+    "final":   "SINCÈREMENT",
+    "pdf_link_text":"Vous avez surement déjà mon CV, au besoin, vous pouvez le télécharger sur :",
+    "pdf_link": "http://alexgalinier.com/static/documents/Alex_Galinier_Resume_Fr_Mail.pdf"
+  },
 ]
